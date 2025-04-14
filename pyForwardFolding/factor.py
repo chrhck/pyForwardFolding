a42d2e7aa21282ede910ce444b457660a30c58e4
Christian Haack <christian.haack@fau.de>
2025-04-14 11:53:30 +0200

    update handling of exposed variables (parameters)

--- pyForwardFolding/factor.py
+++ pyForwardFolding/factor.py
@@ -5,46 +5,61 @@ import numpy as np
 
 class AbstractFactor:
     """
-    Abstract class representing a factor in a forward folding model.
+    Abstract class representing a per-event factor.
     """
+    factor_parameters: List[str] = []
+
+    def __init__(self, name:str, param_mapping: Dict[str, str] = None):
+        self.name = name
+
+        if param_mapping is None:
+            param_mapping = {par: par for par in self.factor_parameters}
+        
+        self.param_mapping = param_mapping
+
+    @property
     def required_variables(self) -> List[str]:
-        raise NotImplementedError
+        return self.req_vars
 
-    def exposed_variables(self) -> List[str]:
-        raise NotImplementedError
-
+    @property
+    def parameter_mapping(self) -> Dict[str, str]:
+        return self.param_mapping
+    
+    
     def evaluate(
         self,
         output: np.ndarray,
         input_variables: Dict[str, Union[np.ndarray, float]],
-        exposed_variables: Dict[str, Union[np.ndarray, float]],
+        parameters: Dict[str, Union[np.ndarray, float]],
     ) -> np.ndarray:
         raise NotImplementedError
 
     @classmethod
     def construct_from(cls: Type["AbstractFactor"], config: Dict[str, Any]) -> "AbstractFactor":
         factor_type = config.get("type")
-        if factor_type == "EnergyScaler":
-            return EnergyScaler(name=config["name"])
-        elif factor_type == "PowerLawFlux":
+        param_mapping = config.get("param_mapping", None)
+        if factor_type == "PowerLawFlux":
             return PowerLawFlux(
                 name=config["name"],
                 pivot_energy=config["pivot_energy"],
                 baseline_norm=config["baseline_norm"],
+                param_mapping=param_mapping,
             )
         elif factor_type == "FluxNorm":
-            return FluxNorm(name=config["name"])
+            return FluxNorm(name=config["name"], param_mapping=param_mapping)
         elif factor_type == "SnowstormGauss":
             return SnowstormGauss(
                 name=config["name"],
                 sys_gauss_width=config["sys_gauss_width"],
                 sys_sim_bounds=tuple(config["sys_sim_bounds"]),
                 req_variable_name=config["req_variable_name"],
+                param_mapping=param_mapping
             )
         elif factor_type == "DeltaGamma":
             return DeltaGamma(
                 name=config["name"],
                 reference_energy=config["reference_energy"],
+                param_mapping=param_mapping
             )
         else:
             raise ValueError(f"Unknown factor type: {factor_type}")
@@ -61,24 +76,25 @@ def get_required_variable_values(factor, input_variable_values):
     Returns:
         dict: Dictionary containing only the required variables for the factor.
     """
-    req_vars = factor.required_variables()
+    req_vars = factor.required_variables
     return {var: input_variable_values[var] for var in req_vars}
 
 
-def get_exposed_variable_values(factor, exposed_variable_values):
+def get_parameter_values(factor, parameter_dict):
     """
-    Extract exposed variable values for a factor from the nested dictionary of exposed variables.
+    Extract parameter values for a factor from the parameter dictionary.
 
     Args:
         factor (AbstractFactor): The factor requesting variables.
-        exposed_variable_values (dict): Dictionary mapping factor names to their exposed variables.
+        parameter_dict (dict): Dictionary mapping parameter names to values.
 
     Returns:
         dict: Dictionary containing only the exposed variables for the factor.
     """
-    this_exposed_variable_values = exposed_variable_values[factor.name]
-    exp_vars = factor.exposed_variables()
-    return {var: this_exposed_variable_values[var] for var in exp_vars}
+
+    par_mapping = factor.parameter_mapping
+    parameter_values = {factor_var_name: parameter_dict[par_name] for factor_var_name, par_name in par_mapping.items()}
+    return parameter_values
 
 
 class PowerLawFlux(AbstractFactor):
@@ -90,20 +106,21 @@ class PowerLawFlux(AbstractFactor):
         pivot_energy (float): Reference energy for the power law.
         baseline_norm (float): Baseline normalization factor.
     """
-    def __init__(self, name, pivot_energy, baseline_norm):
-        self.name = name
+
+    factor_parameters: List[str] = ["flux_norm", "spectral_index"]
+    req_vars: List[str] = ["true_energy"]
+
+    def __init__(self, name:str, pivot_energy, baseline_norm, param_mapping: Dict[str, str]=None):
+        
+        super().__init__(name, param_mapping)
+
         self.pivot_energy = pivot_energy
         self.baseline_norm = baseline_norm
 
-    def required_variables(self):
-        return ["true_energy"]
 
-    def exposed_variables(self):
-        return ["flux_norm", "spectral_index"]
-
-    def evaluate(self, input_variables, exposed_variables):
+    def evaluate(self, input_variables, parameter_values):
         input_values = get_required_variable_values(self, input_variables)
-        exposed_values = get_exposed_variable_values(self, exposed_variables)
+        exposed_values = get_parameter_values(self, parameter_values)
         true_energy = input_values["true_energy"]
         flux_norm = exposed_values["flux_norm"]
         spectral_index = exposed_values["spectral_index"]
@@ -118,17 +135,15 @@ class FluxNorm(AbstractFactor):
     Args:
         name (str): Identifier for the factor.
     """
-    def __init__(self, name):
-        self.name = name
 
-    def required_variables(self):
-        return []
+    factor_parameters: List[str] = ["flux_norm"]
+    required_variables: List[str] = []
 
-    def exposed_variables(self):
-        return ["flux_norm"]
+    def __init__(self, name:str, param_mapping: Dict[str, str] = None):
+        super().__init__(name, param_mapping)
 
     def evaluate(self, input_variables, exposed_variables):
-        exposed_values = get_exposed_variable_values(self, exposed_variables)
+        exposed_values = get_parameter_values(self, exposed_variables)
         flux_norm = exposed_values["flux_norm"]
 
         return flux_norm
@@ -144,23 +159,22 @@ class SnowstormGauss(AbstractFactor):
         sys_sim_bounds (tuple): Bounds for the simulated parameter space (min, max).
         req_variable_name (str): Name of the required variable for reweighting.
     """
-    def __init__(self, name, sys_gauss_width, sys_sim_bounds, req_variable_name):
-        self.name = name
+
+    factor_parameters: List[str] = ["scale"]
+
+    def __init__(self, name, sys_gauss_width, sys_sim_bounds, req_variable_name, param_mapping: Dict[str, str] = None):
+        super().__init__(name, param_mapping)
+
         self.sys_gauss_width = sys_gauss_width
         self.sys_sim_bounds = sys_sim_bounds
-        self.req_variable_name = req_variable_name
+        self.req_vars = [req_variable_name]
 
-    def required_variables(self):
-        return [self.req_variable_name]
-
-    def exposed_variables(self):
-        return ["sys_value"]
 
     def evaluate(self, input_variables, exposed_variables):
         input_values = get_required_variable_values(self, input_variables)
-        exposed_values = get_exposed_variable_values(self, exposed_variables)
-        sys_value = exposed_values["sys_value"]
-        sys_par = input_values[self.req_variable_name]
+        exposed_values = get_parameter_values(self, exposed_variables)
+        sys_value = exposed_values["scale"]
+        sys_par = input_values[self.req_vars[0]]
 
         return (
             (
@@ -180,19 +194,18 @@ class DeltaGamma(AbstractFactor):
         name (str): Identifier for the factor.
         reference_energy (float): Reference energy for scaling.
     """
-    def __init__(self, name, reference_energy):
-        self.name = name
+
+    factor_parameters: List[str] = ["delta_gamma"]
+    req_vars: List[str] = ["true_energy"]
+
+    def __init__(self, name, reference_energy, param_mapping: Dict[str, str] = None):
+        super().__init__(name, param_mapping)
         self.reference_energy = reference_energy
 
-    def required_variables(self):
-        return ["true_energy"]
-
-    def exposed_variables(self):
-        return ["delta_gamma"]
 
     def evaluate(self, input_variables, exposed_variables):
         input_values = get_required_variable_values(self, input_variables)
-        exposed_values = get_exposed_variable_values(self, exposed_variables)
+        exposed_values = get_parameter_values(self, exposed_variables)
 
         delta_gamma = exposed_values["delta_gamma"]
         true_energy = input_values["true_energy"]
