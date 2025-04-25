a3ce01be44beb594037183f0b2bb9cc7c6c4b698
Christian Haack <christian.haack@tum.de>
2025-04-25 11:50:57 +0200

    dev

--- pyForwardFolding/clustering.py
+++ pyForwardFolding/clustering.py
@@ -1,8 +1,8 @@
 import numpy as np
-from sklearn.cluster import HDBSCAN
+from sklearn.cluster import HDBSCAN, MiniBatchKMeans
 
 
-def compress_hdbscan(data, observables, weight_keys, min_cluster_size=5):
+def compress_hdbscan(data, weight_keys, min_cluster_size=5):
     X = np.vstack([data[obs] for obs in observables]).T
     hdb = HDBSCAN(min_cluster_size=min_cluster_size)
     hdb.fit(X)
@@ -29,3 +29,25 @@ def compress_hdbscan(data, observables, weight_keys, min_cluster_size=5):
 
     return compressed
 
+def compress_minibatch_kmeans(data, weight_keys, compression_factor=10):
+
+    n_clusters = int(np.ceil(len(next(iter(data.values()))) / compression_factor))
+
+    X = np.vstack([data[key] for key in data.keys() if key not in weight_keys]).T
+    kmeans = MiniBatchKMeans(n_clusters=n_clusters)
+    kmeans.fit(X)
+
+    cluster_labels = kmeans.labels_
+    cluster_centers = kmeans.cluster_centers_
+
+    compressed = {dkey: np.zeros(n_clusters) for dkey in data.keys()}
+
+    key_ix = 0
+    for dkey in data.keys():
+        if dkey in weight_keys:
+            compressed[dkey] = np.bincount(cluster_labels, weights=data[dkey], minlength=n_clusters)
+        else:        
+            compressed[dkey] = cluster_centers[:, key_ix]
+            key_ix += 1
+
+    return compressed
