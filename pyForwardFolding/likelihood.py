e23547306169c5001046cdb7708675a6008ef423
Christian Haack <christian.haack@tum.de>
2025-07-04 12:54:00 +0200

    implemented SAY likelihood

--- pyForwardFolding/likelihood.py
+++ pyForwardFolding/likelihood.py
@@ -10,8 +10,19 @@ class AbstractLikelihood:
     """
     Abstract base class representing a likelihood function to be evaluated against observed data.
     """
+
+    def __init__(self, analysis: Analysis):
+        self.analysis = analysis
+
+
     def get_analysis(self) -> Analysis:
-        raise NotImplementedError
+        """
+        Get the analysis associated with this likelihood.
+
+        Returns:
+            Analysis: The associated analysis.
+        """
+        return self.analysis
 
     def llh(
         self,
@@ -31,17 +42,8 @@ class PoissonLikelihood(AbstractLikelihood):
         analysis (Analysis): The analysis to evaluate against observed data.
     """
     def __init__(self, analysis: Analysis):
-        self.analysis = analysis
-
-    def get_analysis(self) -> Analysis:
-        """
-        Get the analysis associated with this likelihood.
-
-        Returns:
-            Analysis: The associated analysis.
-        """
-        return self.analysis
-
+        super().__init__(analysis)
+       
     def llh(
         self,
         observed_data: Dict[str, np.ndarray],
@@ -111,3 +113,72 @@ class GaussianUnivariatePrior(AbstractPrior):
         for par, (mean, std) in self.prior_params.items():
             llh += (exposed_parameters[par] - mean)**2 / std**2
         return llh
+
+
+class SAYLikelihood(AbstractLikelihood):
+    """
+    Extension of the Poisson likelihood to account for limited MC statistics.
+
+    https://doi.org/10.48550/arXiv.1901.04645    
+    """
+
+    def __init__(self, analysis: Analysis):
+        self.analysis = analysis
+
+    def llh(
+        self,
+        observed_data: Dict[str, np.ndarray],
+        datasets: Dict[str, Dict[str, Union[np.ndarray, float]]],
+        exposed_variables: Dict[str, Dict[str, Union[np.ndarray, float]]],
+        empty_bins: str = "skip",
+    ) -> float:
+        # Evaluate the analysis
+        ana_eval, ana_eval_ssq = self.analysis.evaluate(datasets, exposed_variables)
+        total_llh = 0.0
+
+        for comp_name, comp_eval in ana_eval.items():
+            obs = observed_data.get(comp_name)
+            if obs is None:
+                raise ValueError(f"No observed data for component '{comp_name}'")
+            
+            
+            non_empty_expectation = comp_eval > 0
+            # Handle empty bins
+            if empty_bins == "throw":
+                if not np.all(non_empty_expectation):
+                    raise ValueError(f"Empty bins in component '{comp_name}'")
+
+            comp_ssq = ana_eval_ssq[comp_name]
+            # Clip SSQ (which could be >mu^2 due to nuisance parameters)
+            comp_ssq = backend.clip(comp_ssq, 0, comp_eval**2)
+
+            sanitized_ssq = backend.select(comp_ssq > 0, comp_ssq, 1E-8)
+            sanizized_mu = backend.select(non_empty_expectation,
+                                          comp_eval,
+                                          1E-8)
+
+            alpha = sanizized_mu**2 / sanitized_ssq + 1.0
+            beta = sanizized_mu / sanitized_ssq
+
+            llh_eff = alpha * backend.log(beta) + backend.gammaln(obs+alpha) - \
+                backend.gammaln(obs + 1) - (obs+alpha)*backend.log(1.+beta) - \
+                backend.gammaln(alpha)
+            
+            llh_poisson =  -sanizized_mu + obs * backend.log(sanizized_mu) - backend.gammaln(obs + 1)
+
+            llh = backend.where(
+                 sanitized_ssq > 0,
+                 llh_eff,
+                 llh_poisson
+            )
+
+            llh_sum = backend.where_sum(
+                non_empty_expectation,
+                llh,
+               0
+            )
+
+            total_llh += llh_sum
+        return total_llh 
+
+     
